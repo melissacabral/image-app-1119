217705bf065ed4884246720d2ea9b800c0ef6c83
Melissa Cabral <melissacabral@gmail.com>
2019-12-03 08:07:37 -0800

    Update single.php

--- single.php
+++ single.php
@@ -51,119 +51,122 @@ if( $_POST['did_comment'] ){
 ?>
 
 <main class="wrapper">
-	<div class="row">
-		<section class="column">
-
-				<?php //get the one post we are trying to view
-				$sql = "SELECT posts.post_id, posts.title, posts.body, posts.date, posts.image, categories.*, users.username
-				FROM posts, categories, users
-				WHERE posts.category_id = categories.category_id
-				AND posts.user_id = users.user_id
-				AND posts.is_published = 1
-				AND posts.post_id = $post_id
-				LIMIT 1"; 
-				//run it
-				$result = $db->query( $sql );
-
-				//check it (are there any rows in the result)
-				if( $result->num_rows >= 1 ){
-					?>
-					<div class="">
-
-						<?php 
-					//loop it
-						while( $post = $result->fetch_assoc() ){ 
-							?>
-
-							<div class="post">
-								<!-- <pre><?php print_r($post); ?></pre> -->
-
-								<img src="<?php echo $post['image']; ?>" 
-								alt="<?php echo $post['title']; ?>" class="post-image">
-								<h3><?php echo $post['title']; ?></h3>
-								<h4><?php echo $post['username']; ?></h4>
-								<h5><?php count_comments( $post['post_id'] ); ?></h5>
-
-								<p><?php echo $post['body']; ?></p>
-
-								<span class="date"><?php human_friendly_date($post['date']); ?></span>
-
-								<span class="category"><?php echo $post['name'] ?></span>					
-							</div>
-					<?php } //end while there are posts 
-					//free it
-					$result->free();
-					?>
-
-				</div>
-
+	<div class="big-section">
+		<div class="row">
+			<section class="column">
+	
+					<?php //get the one post we are trying to view
+					$sql = "SELECT posts.post_id, posts.title, posts.body, posts.date, posts.image, categories.*, users.username
+					FROM posts, categories, users
+					WHERE posts.category_id = categories.category_id
+					AND posts.user_id = users.user_id
+					AND posts.is_published = 1
+					AND posts.post_id = $post_id
+					LIMIT 1"; 
+					//run it
+					$result = $db->query( $sql );
+	
+					//check it (are there any rows in the result)
+					if( $result->num_rows >= 1 ){
+						?>
+						<div class="">
+	
+							<?php 
+						//loop it
+							while( $post = $result->fetch_assoc() ){ 
+								?>
+	
+								<div class="post">
+									<!-- <pre><?php print_r($post); ?></pre> -->
+	
+									<img src="<?php echo $post['image']; ?>" 
+									alt="<?php echo $post['title']; ?>" class="post-image">
+									<h3><?php echo $post['title']; ?></h3>
+									<h4><?php echo $post['username']; ?></h4>
+									<h5><?php count_comments( $post['post_id'] ); ?></h5>
+	
+									<p><?php echo $post['body']; ?></p>
+	
+									<span class="date"><?php human_friendly_date($post['date']); ?></span>
+	
+									<span class="category"><?php echo $post['name'] ?></span>					
+								</div>
+						<?php } //end while there are posts 
+						//free it
+						$result->free();
+						?>
+	
+					</div>
+	
+					<?php 
+				}else{
+						//no rows found in the result
+					echo 'Sorry, no recent posts.';
+				} //end if rows are found ?>
+	
+			</section>
+	
+			<section class="column" id="leave-comment">
+	
+	
 				<?php 
-			}else{
-					//no rows found in the result
-				echo 'Sorry, no recent posts.';
-			} //end if rows are found ?>
-
-		</section>
-
-		<section class="column" id="leave-comment">
-
-
-			<?php 
-			//get all the approved comments on THIS post
-			$sql = "SELECT users.username, users.user_id, comments.body, comments.date
-					FROM comments, users
-					WHERE comments.user_id = users.user_id
-						AND comments.is_approved = 1
-						AND comments.post_id = $post_id
-					ORDER BY comments.date DESC
-					LIMIT 10";
-			//run it
-			$result = $db->query($sql);
-			//check it (twice)
-			if( ! $result ){
-				echo $db->error;
-			}
-			if( $result->num_rows >= 1 ){
-			?>
-			<div class="comments">
-				<h3>Comments:</h3>
-				
-				<?php //loop it
-				while( $comment = $result->fetch_assoc() ){ ?>
-				<div class="one-comment">
-					<h4><?php echo $comment['username']; ?></h4>
-					<p><?php echo $comment['body']; ?></p>
-					<span class="date">
-						<?php human_friendly_date($comment['date']); ?>	
-					</span>
-					<hr>
+				//get all the approved comments on THIS post
+				$sql = "SELECT users.username, users.profile_pic, users.user_id, comments.body, comments.date
+						FROM comments, users
+						WHERE comments.user_id = users.user_id
+							AND comments.is_approved = 1
+							AND comments.post_id = $post_id
+						ORDER BY comments.date DESC
+						LIMIT 5";
+				//run it
+				$result = $db->query($sql);
+				//check it (twice)
+				if( ! $result ){
+					echo $db->error;
+				}
+				if( $result->num_rows >= 1 ){
+				?>
+				<div class="comments">
+					<h3>Comments:</h3>
+					
+					<?php //loop it
+					while( $comment = $result->fetch_assoc() ){ ?>
+					<div class="one-comment">
+						<img width="50" height="50" class="comment-profilepic float-left" src="https://<?php echo $comment['profile_pic'] ?>"><h4><?php echo $comment['username']; ?></h4>
+						
+						<span class="date">
+							<?php human_friendly_date($comment['date']); ?>	
+						</span>
+						<p><?php echo $comment['body']; ?></p>
+						<hr>
+					</div>
+					<?php } //end while ?>
+	
 				</div>
-				<?php } //end while ?>
-
-			</div>
-			<?php } //end if num_rows ?>
-
-
-
-			<?php 
-			if( isset( $feedback ) )
-				feedback_display( $feedback, $feedback_class, $errors ); 
-			?>
-
-			<form action="<?php echo $_SERVER['REQUEST_URI']; ?>#leave-comment" method="post">
-				<label for="the_comment">Your Comment:</label>
-				<textarea name="body" id="the_comment"></textarea>
-
-				<input type="submit" value="Comment">
-
-				<input type="hidden" name="did_comment" value="1">
-
-				<?php //TODO: remove this so it works with the login system ?>
-				<input type="hidden" name="user_id" value="2">
-			</form>
-		</section>
-
-	</div>
+				<?php } //end if num_rows ?>
+	
+	
+	
+				<?php 
+				if( isset( $feedback ) )
+					feedback_display( $feedback, $feedback_class, $errors ); 
+				?>
+	
+				<form action="<?php echo $_SERVER['REQUEST_URI']; ?>#leave-comment" method="post">
+					<label for="the_comment">Your Comment:</label>
+					<textarea name="body" id="the_comment"></textarea>
+	
+					<input type="submit" value="Comment">
+	
+					<input type="hidden" name="did_comment" value="1">
+	
+					<?php //TODO: remove this so it works with the login system ?>
+					<input type="hidden" name="user_id" value="2">
+				</form>
+			</section>
+	
+		</div><!-- .row -->
+	</div><!-- .big-section -->
 
 
 </main>
